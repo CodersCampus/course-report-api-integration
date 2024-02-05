6bb04de506fa8dfdd7fbbd3d80ba4b3683fb1b45
Trevor Page <trevor@craftycodr.com>
2024-02-05 13:29:23 -0500

    initial commit

---
+++ src/main/java/com/coderscampus/coursereportapiintegration/CourseReportApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.coursereportapiintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseReportApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseReportApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/coursereportapiintegration/CourseReportApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.coursereportapiintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseReportApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
