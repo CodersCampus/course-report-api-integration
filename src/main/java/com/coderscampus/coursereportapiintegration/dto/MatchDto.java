0f6c94b6b6bb1dbe7806a050f1e1d8b3aae766fb
Trevor Page <trevor@craftycodr.com>
2024-02-06 15:48:30 -0500

    09 - Parsing a String to a LocalDateTime From an API Response

--- src/main/java/com/coderscampus/coursereportapiintegration/dto/MatchDto.java
+++ src/main/java/com/coderscampus/coursereportapiintegration/dto/MatchDto.java
@@ -1,6 +1,13 @@
 package com.coderscampus.coursereportapiintegration.dto;
 
+import com.coderscampus.coursereportapiintegration.config.CustomLocalDateTimeDeserializer;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.OptBoolean;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+
+import java.time.LocalDateTime;
 
 public record MatchDto(
         @JsonProperty("full_name")
@@ -13,7 +20,8 @@ public record MatchDto(
         String location,
         boolean online,
         @JsonProperty("created_at")
-        String createdAt,
+        @JsonDeserialize(using = CustomLocalDateTimeDeserializer.class )
+        LocalDateTime createdAt,
         String track
 ) {
 }
