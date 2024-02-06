3507ceb1103eb71416fe6d1aba3b46d40f4a8301
Trevor Page <trevor@craftycodr.com>
2024-02-06 09:33:04 -0500

    07 - Using a Fixed Rate Scheduler or Cronjob

--- src/main/java/com/coderscampus/coursereportapiintegration/CourseReportApiIntegrationApplication.java
+++ src/main/java/com/coderscampus/coursereportapiintegration/CourseReportApiIntegrationApplication.java
@@ -3,14 +3,15 @@ package com.coderscampus.coursereportapiintegration;
 import com.coderscampus.coursereportapiintegration.service.CourseReportService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CourseReportApiIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseReportApiIntegrationApplication.class, args);
-		CourseReportService service = new CourseReportService();
-		service.pollCourseReportApi();
+
 
 	}
 
