5fbfdab93efbcad466d276f80e0a20421e7b17c6
Trevor Page <trevor@craftycodr.com>
2024-02-08 06:32:42 -0500

    13 - Fixing a LocalDateTime Parsing Bug

--- src/main/java/com/coderscampus/coursereportapiintegration/config/CustomLocalDateTimeDeserializer.java
+++ src/main/java/com/coderscampus/coursereportapiintegration/config/CustomLocalDateTimeDeserializer.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
 import java.util.Locale;
 
 public class CustomLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
@@ -17,10 +18,22 @@ public class CustomLocalDateTimeDeserializer extends JsonDeserializer<LocalDateT
             .appendPattern("MMMM d, yyyy 'at' h:mma 'UTC'")
             .toFormatter(Locale.ENGLISH);
 
+    private static final DateTimeFormatter FORMATTER_2 = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive() // Make parsing case insensitive
+            .appendPattern("MMMM d, yyyy 'at'h:mma 'UTC'")
+            .toFormatter(Locale.ENGLISH);
+
     @Override
     public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         String date = jsonParser.getText();
         // Preprocess the string if needed, or directly parse if the formatter is set correctly
-        return LocalDateTime.parse(date, FORMATTER);
+        LocalDateTime parsedDateTime = null;
+        try {
+            parsedDateTime = LocalDateTime.parse(date, FORMATTER);
+        } catch (DateTimeParseException e) {
+            parsedDateTime = LocalDateTime.parse(date, FORMATTER_2);
+        }
+
+        return parsedDateTime;
     }
 }
