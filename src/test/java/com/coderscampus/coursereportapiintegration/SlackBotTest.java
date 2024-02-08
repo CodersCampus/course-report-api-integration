c43eabe3569b1e1214fb0e58fa4fcb6a2ca102ab
Trevor Page <trevor@craftycodr.com>
2024-02-08 09:27:22 -0500

    15 - Creating a Slack Bot and Integrating it into a Java App

---
+++ src/test/java/com/coderscampus/coursereportapiintegration/SlackBotTest.java
@@ -0,0 +1,13 @@
+package com.coderscampus.coursereportapiintegration;
+
+import com.coderscampus.coursereportapiintegration.service.SlackBot;
+import org.junit.jupiter.api.Test;
+
+public class SlackBotTest {
+
+
+    @Test
+    public void slack_post_message_test () {
+        SlackBot.postMessage("Test message 2");
+    }
+}
